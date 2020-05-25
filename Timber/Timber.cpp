dc13d3a18272aebe5e159fc55dc3dd56b46090d3
Eliram <eliram13@gmail.com>
2020-05-25 20:27:34 +0900

    intermidiate commit

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -3,6 +3,7 @@
 #include "pch.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include<cassert>
 #include <vector>
 #include <sstream>
 
@@ -10,6 +11,8 @@
 
 
 using namespace sf;
+
+enum side { LEFT, RIGHT, NONE, BOTH };
 
  class SpriteHolder{
 	protected:
@@ -35,6 +38,18 @@ using namespace sf;
    FloatRect textRect = text.getLocalBounds();
    text.setOrigin(textRect.width / 2, textRect.height / 2);
  }
+
+ class Axe : public SpriteHolder {
+ public:
+   Axe(const Texture & textureAxe);
+   void moveRight();
+   void moveLeft();
+
+ private:
+   //line up axe with tree
+   const float AXE_POSITION_LEFT = 700;
+   const float AXE_POISITON_RIGHT = 1075;
+ };
 
 class Bee : public SpriteHolder{
 	private:
@@ -138,6 +153,55 @@ class Cloud : public SpriteHolder {
 
 int Cloud::numberOfClouds = 0;
 
+class Score {
+  public:
+    void reset();
+    void increment();
+    int getScore() const;
+
+ private:
+   int _score = 0;
+};
+
+class Player : public SpriteHolder {
+public:
+  Player(const Texture & texturePlayer);
+  void reset();
+  void update(const Time & deltaTime, Score & score);
+  void setAxe(Axe & axe) {
+    _axe = &axe;
+  }
+  side getSide() const{
+    return _playerSide;
+  }
+
+private:
+  side _playerSide = side::LEFT;
+  bool _acceptInput = true;
+  Axe * _axe = nullptr;
+
+  void chopRight();
+  void chopLeft();
+};
+
+class Log : public SpriteHolder {
+ public:
+   Log(const Texture & textureLog);
+   void flyRight();
+   void flyLeft();
+   void update(const Time & deltaTime) {
+     if (active) {
+       setPosition(sprite.getPosition().x + speedX * deltaTime.asSeconds(), sprite.getPosition().y + speedY * deltaTime.asSeconds());
+     }
+   }
+
+ private:
+   bool active = false;
+   float speedX = 1000;
+   float speedY = -1500;
+};
+
+
 class TimeBar{
  private:
     RectangleShape _bar; 
@@ -166,12 +230,26 @@ class TimeBar{
     _bar.setSize(Vector2f(_bar.getSize().x - timeBarConsumedPerSecond * deltaTime.asSeconds(), _bar.getSize().y));
   }
 
+  void increment(const int score) {
+    _bar.setSize(Vector2f(_bar.getSize().x + 0.15 * timeBarConsumedPerSecond * (2.0 / score), _bar.getSize().y));
+  }
+
   bool timeIsUp() {
     return _bar.getSize().x <= 0.0f;
   }
 
 
 };
+
+
+void updateBranches(const int seed, Sprite * branches, side * branchPositions);
+int main();
+void setUpBranches(Sprite * branches, side * branchPositions);
+
+const int NUMBER_OF_BRANCHES = 6;
+
+
+
 
 
 int main()
@@ -182,35 +260,62 @@ int main()
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 	window.setMouseCursorVisible(false);
 	//create a texture to hold a graphic on the GPU
-	Texture textureBackground;
-	Texture textureTree;
-	Texture textureBee;
-	Texture textureCloud;
+  Texture textureAxe;
+  Texture textureBackground;
+  Texture textureBee;
+  Texture textureBranch;
+  Texture textureCloud;
+  Texture texturePlayer;
+  Texture textureLog;
+  Texture textureRIP;
+  Texture textureTree;
+	
+
 	Font font;
 	
 	//Load a graphic into the texture
+  textureAxe.loadFromFile("graphics/axe.png");
 	textureBackground.loadFromFile("graphics/background.png");
+  textureBranch.loadFromFile("graphics/branch.png");
 	textureTree.loadFromFile("graphics/tree.png");
 	textureBee.loadFromFile("graphics/bee.png");
 	textureCloud.loadFromFile("graphics/cloud.png");
+  texturePlayer.loadFromFile("graphics/player.png");
+  textureLog.loadFromFile("graphics/log.png");
+  textureRIP.loadFromFile("graphics/rip.png");
   font.loadFromFile("Fonts/KOMIKAP_.ttf");
 	  
 	/****************
 	staging area
 	****************/
-  int score = 0;
+  Score score;
 	Sprite spriteBackground;
 	Sprite spriteTree;
+  Sprite spriteRIP;
 	Bee bee{ textureBee };
 	const int numberOfClouds = 3;
 	std::vector<Cloud> clouds(numberOfClouds, Cloud(textureCloud));
 	Text startGameText;
 	Text scoreText;
   TimeBar timeBar;
+  Sprite branches[NUMBER_OF_BRANCHES];
+  side branchPositions[NUMBER_OF_BRANCHES];
+  Player player(texturePlayer);
+  Axe axe(textureAxe);
+  player.setAxe(axe);
+  Log log(textureLog);
+
 	//attach texture to sprite
+  for (int i = 0; i < NUMBER_OF_BRANCHES; ++i) {
+    branches[i].setTexture(textureBranch);
+    FloatRect branchSize = branches[i].getLocalBounds();
+    branches[i].setOrigin(Vector2f(branchSize.width / 2, branchSize.height / 2));
+
+  }
 	spriteBackground.setTexture(textureBackground);
 	spriteTree.setTexture(textureTree);
-	
+  spriteRIP.setTexture(textureRIP);
+
 	//timeBar
 	
 
@@ -233,16 +338,20 @@ int main()
 	//set the Background to the screen 
 	spriteBackground.setPosition(0, 0);
 	spriteTree.setPosition(810, 0);
+  spriteRIP.setPosition(600, 2000);
 
-	//set random seed
 
 
 	sf::Clock clock;
 	bool isGamePaused = true;
 
 
+  for (int i = 0; i < NUMBER_OF_BRANCHES; ++i) {
+    branchPositions[i] = side::NONE;
+  }
 
-
+  int scoreLastCycle = score.getScore();
+  auto isChoped = [&]() {return !(score.getScore() == scoreLastCycle);};
 	//main loop
 	while (window.isOpen()) {
 		/********************************
@@ -254,7 +363,15 @@ int main()
 		if (isGamePaused == true && Keyboard::isKeyPressed(Keyboard::Enter)) {
 			isGamePaused = false;
       timeBar.reset();
-      score = 0;
+      score.reset();
+    //remove branches  
+      for (int i = 0; i < NUMBER_OF_BRANCHES; ++i) {
+        branchPositions[i] = side::NONE;
+      }
+      //remove graveStone
+      spriteRIP.setPosition(675, 2000);
+      
+      player.reset();
 
 		}
 		/********************************
@@ -262,7 +379,15 @@ int main()
 		********************************/
 		Time deltaTime = clock.restart();
 		if (!isGamePaused) {
-			//move bee
+      player.update(deltaTime, score);
+      if (isChoped()) {
+        timeBar.increment(score.getScore()); //increase time
+        player.getSide() == side::LEFT ? log.flyRight() : log.flyLeft(); //send log flying
+        updateBranches((int)time(0), branches, branchPositions);
+
+      }
+      log.update(deltaTime);
+      //move bee
 			bee.update(deltaTime);
       //move clouds
 			for (Cloud & cloud : clouds) {
@@ -277,20 +402,41 @@ int main()
         setTextOriginToCenter(startGameText);
       }
 		}
-		scoreText.setString("score =" + score);
+		scoreText.setString("score =" + score.getScore());
 
+    setUpBranches(branches, branchPositions);
+    scoreLastCycle = score.getScore();
 
 		/********************************
 		Draw the scene
 		********************************/
 		window.clear();
+
+    //darw background
 		window.draw(spriteBackground);
+
 		//draw clouds
 		for (Cloud & cloud : clouds) {
 			cloud.draw(window);
 		}
+
+    //draw tree
 		window.draw(spriteTree);
+
+    //draw bee
 		bee.draw(window);
+
+    //draw branches
+    for (int i = 0; i < NUMBER_OF_BRANCHES; ++i) {
+      window.draw(branches[i]);
+    }
+
+    player.draw(window);
+    axe.draw(window);
+    window.draw(spriteRIP);
+    log.draw(window);
+
+    //draw text
 		window.draw(scoreText);
 		if (isGamePaused) {
 			window.draw(startGameText);
@@ -300,7 +446,7 @@ int main()
     }
 
 
-		//draw everything
+
 		//show everthing
 		window.display();
 	}
@@ -317,3 +463,132 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void setUpBranches(Sprite * branches, side * branchPositions) {
+  for (int i = 0; i < NUMBER_OF_BRANCHES; ++i) {
+    float height = i * 150;
+    switch (branchPositions[i]) {
+      case side::LEFT:
+        branches[i].setPosition(610, height);
+        branches[i].setRotation(180);
+        break;
+      case side::RIGHT:
+        branches[i].setPosition(1330, height);
+        branches[i].setRotation(0);
+      default:
+        //hides branch
+        branches[i].setPosition(-3000, height);
+    }
+  }
+
+}
+
+void updateBranches(const int seed, Sprite *branches, side * branchPositions) {
+  //move branches down 
+  for (int i = 0; i < NUMBER_OF_BRANCHES - 1; ++i) {
+    branchPositions[i] = branchPositions[i + 1];
+  }
+  srand(seed);
+  int r = rand() % 5;
+  if (r == 0) {
+    branchPositions[NUMBER_OF_BRANCHES - 1] = side::LEFT;
+  }else if (r <= 1) {
+    branchPositions[NUMBER_OF_BRANCHES - 1] = side::RIGHT;
+  }
+  else {
+    branchPositions[NUMBER_OF_BRANCHES - 1] = side::NONE;
+  }
+}
+
+Player::Player(const Texture & texturePlayer) : SpriteHolder{texturePlayer}
+{
+  sprite.setPosition(580, 720);
+}
+
+void Player::reset()
+{
+  setPosition(580, 720);
+  _playerSide = side::LEFT;
+  _acceptInput = true;
+}
+
+void Player::update(const Time & deltaTime, Score & currentScore)
+{
+  assert(_axe != nullptr && "The axe has no been set!");
+  if (!_acceptInput) return;
+  if (Keyboard::isKeyPressed(Keyboard::Right)) {
+    chopRight();
+    currentScore.increment();
+    _acceptInput = false;
+  }
+  else if (Keyboard::isKeyPressed(Keyboard::Left)) {
+    chopLeft();
+    currentScore.increment();
+    _acceptInput = false;
+  }
+}
+
+void Player::chopRight()
+{
+  _playerSide = side::RIGHT;
+  _axe->moveRight();
+  setPosition(1200, 720);
+}
+
+void Player::chopLeft()
+{
+  _playerSide = side::LEFT;
+  _axe->moveLeft();
+  setPosition(580, 720);
+}
+
+Axe::Axe(const Texture & textureAxe) : SpriteHolder{ textureAxe }
+{
+  setPosition(700, 830);
+}
+
+void Axe::moveRight()
+{
+  setPosition(AXE_POISITON_RIGHT, this->sprite.getPosition().y);
+}
+
+void Axe::moveLeft()
+{
+  setPosition(AXE_POSITION_LEFT, this->sprite.getPosition().y);
+}
+
+Log::Log(const Texture & textureLog) : SpriteHolder{textureLog}
+{
+  setPosition(810, 720);
+}
+
+void Log::flyRight()
+{
+  setPosition(810, 720);
+  speedX = 5000;
+  active = true;
+}
+
+void Log::flyLeft()
+{
+  setPosition(810, 720);
+  speedX = -5000;
+  active = true;
+}
+
+void Score::reset()
+{
+  _score = 0;
+}
+
+void Score::increment()
+{
+  ++_score;
+}
+
+int Score::getScore() const
+{
+  return _score;
+}
+
+
